ef7b5a0178cf6e545815d11d3349b7667b236a4c
xSarscov <vargasernest7@gmail.com>
2024-01-27 00:50:13 -0600

    Add Post, Like and Comment models :bat:

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Post, Like, Comment
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
--- network/models.py
+++ network/models.py
@@ -4,3 +4,27 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} - {self.timestamp}'
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_liked = models.ForeignKey(Post, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} likes {self.post_liked.content} at {self.timestamp}'
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
